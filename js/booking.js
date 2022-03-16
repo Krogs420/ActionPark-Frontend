cd59f63847ad770f7f2702de1902225808a346ae
August <Augusthauerslev@outlook.com>
2022-03-16 12:33:50 +0100

    Buttons now seperate

--- js/booking.js
+++ js/booking.js
@@ -1,29 +1,36 @@
-const times = ["08:00","09:00", "10:00","11:00", "12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00", "22:00","23:00"];
-const out = function (str){
+const times = ["08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"];
+const out = function (str) {
   console.log(str)
 }
 
-const pbCreateTimeTable = document.getElementById("pbCreateTimeTable")
-
-$(document).ready(function() {
-  $('.fa-calendar').click(function(){
-    $(document).ready(function(){
+$(document).ready(function () {
+  $('.fa-calendar').click(function () {
+    $(document).ready(function () {
       $("#document_date").datepicker().focus();
     });
   });
 });
 
-
-let txt = "";
-for (let x in times) {
-  txt += times[x] + " ";
-  document.getElementById("times").innerHTML = txt;
+function addRow() {
+  for ( let x of times){
+    let timeName = document.createElement("button");
+    timeName.style.backgroundColor = "Green"
+    const tidspunkt = document.createTextNode(x);
+    timeName.append(tidspunkt);
+    timeTable.append(timeName);
+  }
 }
 
+function createTimeTableFromMap() {
+  out("create table");
+  timeMap.forEach(times => addRow(times)
+  )
+}
 
+const pbCreateTimeTable = document.getElementById("pbCreateTimeTable");
+const timeTable = document.getElementById("timetable")
 
-
-pbCreateTimeTable-addEventListener('click', createTableFromMap)
+pbCreateTimeTable.addEventListener('click', addRow)
 
 
 
