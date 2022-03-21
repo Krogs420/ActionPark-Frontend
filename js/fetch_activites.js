29dbd0f618f100f9e4afcad35ce02b1c58d3c012
Mohamad Ibrahim <mohamedibr@gmail.com>
2022-03-21 18:28:25 +0100

    added id to html elements

--- js/fetch_activites.js
+++ js/fetch_activites.js
@@ -17,6 +17,7 @@ async function createActivities() {
     let parent = document.getElementById("parent")
     let activityName = document.createElement("div");
     activityName.classList.add("activity-box");
+    activityName.setAttribute('id', x.activityId)
     parent.append(activityName)
     let activityinfo = document.createElement("div");
     activityinfo.classList.add("activity-info")
@@ -33,48 +34,49 @@ async function createActivities() {
 
     let timeTable = document.createElement("div");
     activityName.append(timeTable);
-    for (let x of times) {
+    for (let i = 0; i < times.length; i++) {
       let timeName = document.createElement("button");
+      timeName.setAttribute('id', i);
       timeName.classList.add("time-button");
-      const tidspunkt = document.createTextNode(x);
+      const tidspunkt = document.createTextNode(times[i]);
       timeName.append(tidspunkt);
       timeTable.append(timeName);
     }
 
 
-      /*
-      activityinfo.append(activityh2);
-      const activityNameTag = document.createTextNode(x);
-      activityName.append(activityNameTag);
-      activityName.addEventListener('click', clickButton)
-      timeTable.append(activityName);
-  */
+    /*
+    activityinfo.append(activityh2);
+    const activityNameTag = document.createTextNode(x);
+    activityName.append(activityNameTag);
+    activityName.addEventListener('click', clickButton)
+    timeTable.append(activityName);
+*/
 
-    }
   }
+}
 
-  function addActivity(activity) {
-    const rowCount = activityTable.rows.length;
-    let row = activityTable.insertRow(rowCount);
-    let colCount = 0;
+function addActivity(activity) {
+  const rowCount = activityTable.rows.length;
+  let row = activityTable.insertRow(rowCount);
+  let colCount = 0;
 
-    //Sætter Aktivitets
-    let cell = row.insertCell(colCount++);
-    cell.innerText = activity.activityName;
+  //Sætter Aktivitets
+  let cell = row.insertCell(colCount++);
+  cell.innerText = activity.activityName;
 
-    //Billede til aktivitet
-    cell = row.insertCell(colCount++);
-    const imgTag = document.createElement('img');
-    imgTag.setAttribute("src", activity.activityImageHref);
-    imgTag.innerText = activity.name;
-    cell.appendChild(imgTag);
-  }
+  //Billede til aktivitet
+  cell = row.insertCell(colCount++);
+  const imgTag = document.createElement('img');
+  imgTag.setAttribute("src", activity.activityImageHref);
+  imgTag.innerText = activity.name;
+  cell.appendChild(imgTag);
+}
 
-  function createActivityMap() {
-    out("create table");
-    activityMap.forEach(activity => createActivities(activity))
-  }
+function createActivityMap() {
+  out("create table");
+  activityMap.forEach(activity => createActivities(activity))
+}
 
-  const button = document.getElementById("btn")
-  const activityTable = document.getElementById("activityTable")
+const button = document.getElementById("btn")
+const activityTable = document.getElementById("activityTable")
 
