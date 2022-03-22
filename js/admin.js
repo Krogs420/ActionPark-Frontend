47ee24b45eac6e7add42d593b2d09ebf031d9b66
Kristian Kryger <kristianlaukryger@gmail.com>
2022-03-22 14:34:27 +0100

    admin.js is a work in progress thats needs help

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,6 @@
 const times = ["08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"];
 const activities = 'http://localhost:8080/api/activity/all-activities';
-const bookingLine = 'http://localhost:8080/api/bookingline/all-booking-lines';
+const bookingLine = 'http://localhost:8080/api/booking-line/all-booking-lines';
 const customer = 'http://localhost:8080/api/customer/all-customers';
 const booking = 'http://localhost:8080/api/booking/all-bookings';
 const bookingOverview = document.getElementById('bview-table')
@@ -30,7 +30,7 @@ async function addTableOverview() {
   for (let x of bookings) {
     const tableRow = document.createElement('tr');
     const td1 = document.createElement('td');
-    td1.textContent = activity.activityName;
+    td1.textContent = x.activityName;
     const td2 = document.createElement('td');
     td2.textContent = activity.toString();
     const td3 = document.createElement('td');
@@ -48,20 +48,6 @@ async function addTableOverview() {
     console.log("fgedgdfg");
   }
 
-}
-
-function addBookinglineToBooking(activity, time) {
-  const tableRow = document.createElement('tr');
-  const td1 = document.createElement('td')
-  td1.textContent = activity.activityName;
-  const td2 = document.createElement('td')
-  td2.textContent = time.toString();
-  const td3 = document.createElement('td')
-  td3.textContent = activity.activityPrice
-  tableRow.append(td1);
-  tableRow.append(td2);
-  tableRow.append(td3);
-  bookingTable.append(tableRow)
 }
 
 addTableOverview(activities, customer, booking, bookingLine);
