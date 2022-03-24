4d723d4ed3a3af13d2f1e7281f1dd08b9f83b6be
Mohamad Ibrahim <mohamedibr@gmail.com>
2022-03-24 16:45:45 +0100

    refactored code, removed file

--- js/fetch_activites.js
+++ js/fetch_activites.js
@@ -1,7 +1,9 @@
+let times = ["08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"];
 const activities = 'http://localhost:8080/api/activity/all-activities';
 const activityTable = document.getElementById("activityTable")
 const bookingTable = document.getElementById('bline-table')
 const bookingBttn = document.getElementById('book-button')
+const timeTable = document.getElementById("timetable")
 const activityArray = []
 
 createActivities().catch(err => console.error(err));
@@ -157,3 +159,13 @@ async function postBookingLine(activity) {
   }
   return response;
 }
+
+
+$(document).ready(function () {
+  $('.fa-calendar').click(function () {
+    $(document).ready(function () {
+      $("#document_date").datepicker().focus();
+    });
+  });
+});
+
